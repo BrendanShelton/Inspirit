e596fc005a48c799279c1ab23a16fbbf1a225e5a
Hshuttle <huntershutle@gmail.com>
2022-11-20 20:23:28 -0500

    updated models folder to include quote.js and seeded a quotes file

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Project = require('./Project');
+const Quote = require('./quote');
 
-User.hasMany(Project, {
+User.hasMany(Quote, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Quote.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Quote };
