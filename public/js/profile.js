bb1eb28f978d2c1b07abf0b01eda829b3a52a451
Brendan Shelton <brendan.d.shelton@gmail.com>
2022-11-27 12:48:29 -0500

    worked on fetches in js and attempted to fix portfolio handlebars

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,23 +1,23 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const quote = document.querySelector('#quote-name').value.trim();
+  const author = document.querySelector('#speaker-author').value.trim();
+  const comment = document.querySelector('#comment-desc').value.trim();
 
   if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+    const response = await fetch(`/api/quotes`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ quote, author }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/portfolio');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create quote');
     }
   }
 };
@@ -26,12 +26,12 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/quotess/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/portfolio');
     } else {
       alert('Failed to delete project');
     }
@@ -39,9 +39,9 @@ const delButtonHandler = async (event) => {
 };
 
 document
-  .querySelector('.new-project-form')
+  .querySelector('#post-quote')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('.quote-list')
   .addEventListener('click', delButtonHandler);
